95562fef1ccff6dd03d2c5b2a2ab27bad07c84c8
jtudaortiz <jorgetuda@gmail.com>
2026-08-31 10:28:36 +0200

    Create 8_AIMSUN_PY_LOS_Urban_control_delay_HCM7th2022_tdemora_INTERVALOS.py

---
+++ 8_AIMSUN_PY_LOS_Urban_control_delay_HCM7th2022_tdemora_INTERVALOS.py
@@ -0,0 +1,51 @@
+
+################### 
+#Script para crear un nuevo atributo en cada sección con el Nivel de Servicio (LOS) a partir del tiempo de demora/demora en segmento (s) de la via urbana
+
+## Demora de cola DYNAMIC::SRC_GKSection_queueDelay_0
+
+## Demora de parada DYNAMIC::SRC_GKSection_stoppedDelay_0
+###################
+
+
+def getCreateAttribute():
+	sectionType = model.getType ("GKSection")
+	attribute_2 = sectionType.addColumn ("GKSection::UR_CONDELAY_LOS","LOS (control_delay)", GKColumn.String)
+	return attribute_2
+		
+
+def fillattribute(column):
+	sectionType = model.getType ("GKSection")
+	con_delay_col = model.getColumn("DYNAMIC::SRC_GKSection_delayTime_0") 
+	index = 0 #Modificar el valor para cambiar el intervalo de toma de datos (0= Primer intervalo)		
+
+	for section in GK.GetObjectsOfType (sectionType) :
+		
+		con_delayTS = section.getDataValueTS(con_delay_col)
+		if con_delayTS:
+
+			con_delay = con_delayTS.getValue(GKTimeSerieIndex(index))[0]
+							
+			if (con_delay <= 10):
+				CON_DELAY_LOS = "A" 
+			elif (con_delay > 10) and (con_delay <= 15):
+				CON_DELAY_LOS = "B"
+			elif (con_delay  > 15) and (con_delay  <= 25):
+				CON_DELAY_LOS = "C"
+			elif (con_delay  > 25) and (con_delay  <= 35):
+				CON_DELAY_LOS = "D"
+			elif (con_delay  > 35) and (con_delay  <= 50):
+				CON_DELAY_LOS = "E"
+			elif (con_delay  > 50 ):
+				CON_DELAY_LOS = "F"
+			else:
+				CON_DELAY_LOS  = "Out of range"
+								
+			section.setDataValue(column, QVariant(CON_DELAY_LOS))
+		else:
+			continue
+	
+attribute_2 = getCreateAttribute()
+fillattribute(attribute_2)
+print ("Demora de parada (control Delay) LOS calculado")
+model.getCommander().addCommand(None)
